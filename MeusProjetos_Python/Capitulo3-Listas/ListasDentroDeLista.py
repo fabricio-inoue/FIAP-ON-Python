e1d49ac8156ab8cc8e3a70f13fd2e06ea8a4d14e
fabricio-inoue <103461034+fabricio-inoue@users.noreply.github.com>
2023-06-24 02:16:05 -0300

    Projetos

--- MeusProjetos_Python/Capitulo3-Listas/ListasDentroDeLista.py
+++ MeusProjetos_Python/Capitulo3-Listas/ListasDentroDeLista.py
@@ -1,5 +1,7 @@
 inventario=[]
 resposta = "S"
+
+# adicionar elementos na lista
 while resposta == "S":
   equipamento=[input("Equipamento: "),
             float(input("Valor: ")),
@@ -8,18 +10,21 @@ while resposta == "S":
   inventario.append(equipamento)
   resposta = input("Digite 'S' para continuar: ").upper()
 
+# exibir os dados da lista
 for elemento in inventario:
   print("Nome.........: ", elemento[0])
   print("Valor........: ", elemento[1])
   print("Serial.......: ", elemento[2])
   print("Departamento.: ", elemento[3])
 
+# buscar elementos na lista
 busca=input("Digite o nome do equipamento que deseja buscar: ")
 for elemento in inventario:
   if busca==elemento[0]:
     print("Valor..: ", elemento[1])
     print("Serial.:", elemento[2])
 
+# depreciar elementos da lista
 depreciacao=input("Digite o nome do equipamento que será depreciado: ")
 for elemento in inventario:
   if depreciacao==elemento[0]:
@@ -27,13 +32,24 @@ for elemento in inventario:
     elemento[1] = elemento[1] * 0.9
     print("Novo valor: ", elemento[1])
 
+# excluir elemento da lista de acordo com o serial
 serial=int(input("Digite o serial do equipamento que será excluído: "))
 for elemento in inventario:
   if elemento[2]==serial:
     inventario.remove(elemento)
 
+# reexibir a lista
 for elemento in inventario:
   print("Nome.........: ", elemento[0])
   print("Valor........: ", elemento[1])
   print("Serial.......: ", elemento[2])
   print("Departamento.: ", elemento[3])
+
+# relatório dos valores
+valores=[]
+for elemento in inventario:
+  valores.append(elemento[1])
+if len(valores)>0:
+  print("O equipamento mais caro custa: ", max(valores))
+  print("O equipamento mais barato custa: ", min(valores))
+  print("O total de equipamentos é de: ", sum(valores))
